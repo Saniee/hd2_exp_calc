002cbb5496339e5f74cdc64e601c79763e1ebe97
Saniee <asamsku10@gmail.com>
2025-06-01 12:32:13 +0200

    Hello World example of GUI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+use gui::AppGui;
+
+mod gui;
+
+#[tokio::main]
+async fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native("hd2_exp_calc", options, Box::new(|_| {
+        Ok(Box::<AppGui>::default())
+    }))
 }
